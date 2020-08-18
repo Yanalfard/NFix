e9e5cd5d3a0e8aafd072b35ee9afacff3234aec0
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-08-18 06:32:24 -0700

    Bmi Calculator Update

---
+++ NFix/Scripts/UIScripts/BMICalculator.js
@@ -0,0 +1,11 @@
+﻿function Calculate(inputId) {
+    debugger;
+    const age = document.getElementById('txtAge').value;
+    const height = parseInt(document.getElementById('txtHeight').value);
+    const weight = parseInt(document.getElementById('txtWeight').value);
+
+    const txt = document.getElementById(inputId);
+
+    txt.innerText = weight / Math.pow(height , 2);
+
+}
