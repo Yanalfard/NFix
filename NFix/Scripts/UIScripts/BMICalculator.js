8a9c512076d81f1f3bc24290b0782666fa2549bf
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-08-27 04:08:37 -0700

    Homepage progress -Sign in and Sign Up complete -Modals implemented

--- NFix/Scripts/UIScripts/BMICalculator.js
+++ NFix/Scripts/UIScripts/BMICalculator.js
@@ -1,11 +1,8 @@
 ﻿function Calculate(inputId) {
-    debugger;
-    const age = document.getElementById('txtAge').value;
     const height = parseInt(document.getElementById('txtHeight').value);
     const weight = parseInt(document.getElementById('txtWeight').value);
 
     const txt = document.getElementById(inputId);
 
-    txt.innerText = weight / Math.pow(height , 2);
-
+    txt.innerText = String(weight / Math.pow(height, 2)).slice(0,5);
 }
