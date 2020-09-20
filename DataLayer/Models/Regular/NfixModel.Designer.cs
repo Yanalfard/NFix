bf0e069dd922f6e59fa1b27cc1afbfea45a83594
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-09-20 00:04:39 -0700

    small view changes

--- DataLayer/Models/Regular/NfixModel.Designer.cs
+++ DataLayer/Models/Regular/NfixModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\YANAL\Documents\Visual Studio 2019\NFix\DataLayer\Models\Regular\NfixModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Lemi\source\repos\NFix\DataLayer\Models\Regular\NfixModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
