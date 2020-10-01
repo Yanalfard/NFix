929c8a7444bc8b2e349269a6c8af4b2336ac5bc0
Mehdi Sahandi <mehdisahandi@gmail.com>
2020-10-01 15:37:29 +0330

    update

---
+++ NFix/Controllers/HomeVideoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFix.Controllers
+{
+    public class HomeVideoController : Controller
+    {
+        // GET: HomeVideo
+        public ActionResult VideosPage()
+        {
+            return View();
+        }
+        public ActionResult VideoView()
+        {
+            return View();
+        }
+    }
+}
