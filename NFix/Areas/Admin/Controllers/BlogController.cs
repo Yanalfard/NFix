524eca0d9a4271277452965de1928562f2451824
Mehdi Sahandi <mehdisahandi@gmail.com>
2020-10-12 13:57:24 +0330

    update blog

--- NFix/Areas/Admin/Controllers/BlogController.cs
+++ NFix/Areas/Admin/Controllers/BlogController.cs
@@ -1,6 +1,11 @@
-﻿using DataLayer.Services.Impl;
+﻿using DataLayer;
+using DataLayer.Models.Dto;
+using DataLayer.Models.Regular;
+using DataLayer.Services.Impl;
+using DataLayer.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,7 +27,27 @@ namespace NFix.Areas.Admin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult BlogAdder(BlogViewModel blog, HttpPostedFileBase MainImage)
+        {
+            if (MainImage != null)
+            {
+                blog.MainImage = Guid.NewGuid().ToString() + Path.GetExtension(MainImage.FileName);
+                MainImage.SaveAs(Server.MapPath("/Resources/Blogs/" + blog.MainImage));
+            }
+            blog.LikeCount = 0;
 
+            _blog.AddBlog(new TblBlog()
+            {
+                Body = blog.Body,
+                Description=blog.Description,
+                LikeCount=blog.LikeCount,
+                MainImage=blog.MainImage,
+                Title=blog.Title,
+                
+            });
+            return View(blog);
+        }
         public ActionResult BlogComments()
         {
             return View();
