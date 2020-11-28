02438fc18679ef9fc3511e580ab6a1c2f348d037
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-28 17:27:38 +0330

    Tweakes and fixes #1

---
+++ NFix/Scripts/UIScripts/CopyToClipboard.js
@@ -0,0 +1,20 @@
+﻿function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+    } catch (err) {
+    }
+
+    document.body.removeChild(textArea);
+}
---
+++ NFix/Scripts/UIScripts/ImageLoadFailed.js
@@ -0,0 +1,10 @@
+﻿const images = document.getElementsByTagName('img');
+for (let image of images) {
+    image.setAttribute("onerror", "imageError(this)")
+}
+
+function imageError(image) {
+    image.onerror = "";
+    image.src = "/Resources/Svg/NoImage.svg";
+    return true;
+}
