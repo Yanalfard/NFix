5677147a9309247b2d02ea4712a2c5890e9b3345
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-12-05 16:27:13 +0330

    VideoView alert box

--- NFix/Scripts/UIScripts/Global.js
+++ NFix/Scripts/UIScripts/Global.js
@@ -18,3 +18,18 @@
     document.body.removeChild(textArea);
     UIkit.notification("لینک صفحه کپی شد")
 }
+
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+if (isInStandaloneMode) {
+}
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+    this.setState({ showInstallMessage: true });
+}
