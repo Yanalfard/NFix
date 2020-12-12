edbe9c599dc494d8527f53835a24f41bfc3835ee
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-12-12 16:36:37 +0330

    Iphone and Safari support

--- NFix/Scripts/UIScripts/Global.js
+++ NFix/Scripts/UIScripts/Global.js
@@ -19,12 +19,60 @@
     UIkit.notification("لینک صفحه کپی شد")
 }
 
-// Detects if device is on iOS 
-const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
+const isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isIos = /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+
+let deferredPrompt;
+const addButtons = document.getElementsByClassName('add-button');
+window.addEventListener('beforeinstallprompt', (e) => {
+    try {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+
+        for (let addBtn of addButtons) {
+            if (isInStandaloneMode) {
+                addBtn.style.display = "none";
+            }
+
+            console.log('oi');
+            addBtn.addEventListener('click', btnDownloadClick);
+        }
+
+    } catch { }
+});
+
+
+function btnDownloadClick(e) {
+
+    if (isInStandaloneMode) {
+        UIkit.notification('برنامه نصب شده است');
+        return;
+    }
+    //Is not installed
+
+    if (isSafari && isIos) {
+        UIkit.modal(document.getElementById('Modal-PopupIos')).show();
+        return;
+    }
+
+    try {
+        deferredPrompt.prompt();
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        deferredPrompt = null;
+    });
 }
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-if (isInStandaloneMode) {
+
+if (isSafari && isIos) {
+    setInterval(() => {
+        UIkit.modal(document.getElementById('Modal-PopupIos')).show();
+    }, 60000 * 2);
 }
--- NFix/Scripts/UIScripts/Index.js
+++ NFix/Scripts/UIScripts/Index.js
@@ -5,49 +5,6 @@
     });
 }
 
-let deferredPrompt;
-const addBtn = document.getElementsByClassName('add-button')[0];
-const addBtn2 = document.getElementsByClassName('add-button')[1];
-window.addEventListener('beforeinstallprompt', (e) => {
-    try {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-
-        addBtn.addEventListener('click', btnDownloadClick);
-        addBtn2.addEventListener('click', btnDownloadClick);
-    } catch { }
-});
-
-function btnDownloadClick(e) {
-
-    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-    if (isInStandaloneMode) {
-        UIkit.notification('برنامه نصب شده است');
-        return;
-    }
-
-    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    if (isSafari) {
-
-        return;
-    }
-
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-        } else {
-            console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-    });
-}
-
-
 const floatingCart = document.getElementsByClassName('floating-cart')[0];
 floatingCart.style.opacity = "0";
 window.addEventListener('scroll', () => {
