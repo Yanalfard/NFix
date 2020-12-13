392d9c4d02b25d41b79090509ea6c5aa07b398ba
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-12-13 10:24:55 +0330

    Footer creds ajustments

--- NFix/Scripts/UIScripts/Global.js
+++ NFix/Scripts/UIScripts/Global.js
@@ -74,5 +74,5 @@ function btnDownloadClick(e) {
 if (isSafari && isIos) {
     setInterval(() => {
         UIkit.modal(document.getElementById('Modal-PopupIos')).show();
-    }, 60000 * 2);
+    }, 60000 * 10);
 }
